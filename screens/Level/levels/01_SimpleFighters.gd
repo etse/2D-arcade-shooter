6fc4183e67d6fa0dcaab2340b9949bc70b10cde2
Steffen Hageland <steffen.hageland@bekk.no>
2019-06-18 13:28:49 +0200

    Flytter spawn-logikk inn i state-treet

---
+++ screens/Level/levels/01_SimpleFighters.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var basicEnemy = preload("res://components/Enemies/BasicEnemy/BasicEnemy.tscn") 
+
+signal spawned_enemy(enemy)
+
+func start():
+	$Timer.start()
+	
+func update():
+	pass
+
+func _on_Timer_timeout():
+	var enemy = basicEnemy.instance()
+	enemy.position = $StartLeft.position	
+	$tween.interpolate_property(enemy, "position", $StartLeft.position, $EndLeft.position, 5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$tween.start()
+	emit_signal("spawned_enemy", enemy)
+	
--- screens/Level/levels/Level1.gd
+++ screens/Level/levels/Level1.gd
@@ -1,17 +1,8 @@
 extends Node2D
 
-var basicEnemy = preload("res://components/Enemies/BasicEnemy/BasicEnemy.tscn") 
-onready var tweenNode = get_node("tween")
-
 signal spawned_enemy(enemy)
 
 func _ready():
 	pass
 
-func _on_Timer_timeout():
-	var enemy = basicEnemy.instance()
-	enemy.position = $StartLeft.position	
-	tweenNode.interpolate_property(enemy, "position", $StartLeft.position, $EndLeft.position, 5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	tweenNode.start()
-	emit_signal("spawned_enemy", enemy)
 	
