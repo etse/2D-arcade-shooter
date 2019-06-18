9ef050eefb5faa13de06d2883247d2e5417ac747
Steffen Hageland <steffen.hageland@bekk.no>
2019-06-18 14:51:59 +0200

    Bytt hvilket spawnpunkt man bruker

--- screens/Level/levels/01_SimpleFighters.gd
+++ screens/Level/levels/01_SimpleFighters.gd
@@ -1,7 +1,9 @@
 extends Node
 
 var basicEnemy = preload("res://components/Enemies/BasicEnemy/BasicEnemy.tscn")
-
+var spawnCount = 0
+onready var startPoints = [$Points.StartLeft, $Points.StartCenter, $Points.StartRight, $Points.StartCenter]
+onready var endPoints = [$Points.EndLeft, $Points.EndCenter, $Points.EndRight, $Points.EndCenter]
 signal spawned_enemy(enemy)
 
 func start():
@@ -12,9 +14,11 @@ func update(delta: float):
 
 func _on_Timer_timeout():
 	var enemy = basicEnemy.instance()
-	enemy.position = $Points.StartLeft.position
-	$tween.interpolate_property(enemy, "position", $Points.StartLeft.position, $Points.EndLeft.position, 5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	var startPos = startPoints[spawnCount % startPoints.size()]
+	var endPos = endPoints[spawnCount % endPoints.size()]
+	enemy.position = startPos.position
+	$tween.interpolate_property(enemy, "position", startPos.position, endPos.position, 5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$tween.start()
-	print("spawn")
 	emit_signal("spawned_enemy", enemy)
+	spawnCount += 1
 	
